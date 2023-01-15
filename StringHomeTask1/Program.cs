b5afe76d535c1667afa388636372cc1f920112ff
kenanxasiyevv <kananakh@code.edu.az>
2023-01-15 22:03:06 +0400

    Add project files.

---
+++ StringHomeTask1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace StringHomeTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(Ceaser.CeaserMethod());
+        }
+    }
+}
